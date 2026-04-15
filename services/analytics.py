6f1d96db530e06471aaf4020154996deecf277a5
Jaryl Ong <jarylosq@gmail.com>
2026-04-15 12:35:49 +0800

    Added accrued interest formula

--- services/analytics.py
+++ services/analytics.py
@@ -18,8 +18,7 @@ def day_count_discount(
         num_days_btw_cpn_dt = (next_cpn_dt - prev_cpn_dt).days
         settle_to_next_coupon = (next_cpn_dt - settlement_dt).days
 
-    w = settle_to_next_coupon / num_days_btw_cpn_dt
-    return w 
+    return settle_to_next_coupon, num_days_btw_cpn_dt 
     
 def calculate_bond_price(
     coupon: float,
@@ -36,16 +35,23 @@ def calculate_bond_price(
     Used to calculate the dirty price of a bond via discounting the cashflows (coupon periods + redemption)
     """
     num_cpn_period: int = cpn_freq * maturity
-    w = day_count_discount(day_count, settlement_dt, prev_cpn_dt, next_cpn_dt)
-    
+    settle_to_next_coupon, num_days_btw_cpn_dt = day_count_discount(day_count, settlement_dt, prev_cpn_dt, next_cpn_dt)
+    w = settle_to_next_coupon / num_days_btw_cpn_dt
     dirty_price = 0
-    for i in range(1, num_cpn_period+1):
-        if i == num_cpn_period+1:
+    for i in range(1, num_cpn_period + 1):
+        if i == num_cpn_period + 1:
             dirty_price += redemption_value / (1 + yield_per_period/100) ** (i-1 + w)
         dirty_price += coupon / (1 + yield_per_period/100) ** (i-1 + w)
     return dirty_price
 
 def calculate_accrued_interest(
-        
+    coupon: float,
+    cpn_freq: int,
+    day_count: str,
+    settlement_dt: date,
+    prev_cpn_dt: date,
+    next_cpn_dt: date    
 ) -> float:
-    return
+    settle_to_next_coupon, num_days_btw_cpn_dt = day_count_discount(day_count, settlement_dt, prev_cpn_dt, next_cpn_dt)
+    accrued_interest = (num_days_btw_cpn_dt - settle_to_next_coupon) / num_days_btw_cpn_dt * coupon / cpn_freq
+    return accrued_interest
